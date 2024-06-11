4dd362ab00e2f6a84f2e0ba7382001da929403a8
Przemyslaw Figura <github.enlarging980@passinbox.com>
2024-06-12 00:52:11 +0200

    make telescope search hiden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,13 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+
+			vim.keymap.set(
+				"n",
+				"<leader>p",
+				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git'}})<cr>",
+				{}
+			)
 			vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
 		end,
 	},
