9db8d674a5a3a92666f01562ac4ec2eae5782129
Przemyslaw Figura <github.enlarging980@passinbox.com>
2024-06-12 11:20:04 +0200

    split mac os and wsl2 configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,22 +2,34 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-set -gx HOMEBREW_NO_ENV_HINTS true
-set -gx EDITOR "nvim"
-set -gx GIT_EDITOR "nvim"
-set -gx PYENV_ROOT "$HOME/.pyenv"
-
-alias cdd="cd $HOME/Developer"
 set fish_greeting
 fish_vi_key_bindings
 
-fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
-fish_add_path "$HOME/.composer/vendor/bin"
+if test (uname) = "Darwin"
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+
+	alias cdd="cd $HOME/Developer"
+
+	set -gx HOMEBREW_NO_ENV_HINTS true
+
+	fish_add_path "$HOME/.composer/vendor/bin"
+	fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+
+	source ~/.orbstack/shell/init.fish 2>/dev/null || :
+end
+
+if test -e "/etc/wsl.conf"
+	alias cdd="cd /projects"
+	fish_add_path "/opt/nvim-linux64/bin"
+	fish_add_path "$HOME/.local/bin"
+end
+
+set -gx EDITOR "nvim"
+set -gx GIT_EDITOR "nvim"
+
+set -gx PYENV_ROOT "$HOME/.pyenv"
 fish_add_path "$PYENV_ROOT/bin"
+pyenv init - | source
 
 source "$HOME/.cargo/env.fish"
-source ~/.orbstack/shell/init.fish 2>/dev/null || :
-pyenv init - | source
 
