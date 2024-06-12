71eb8c61379d511b5d64368f3c957fca67225c19
Przemyslaw Figura <github.enlarging980@passinbox.com>
2024-06-12 21:55:05 +0200

    fix fnm config

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -1,5 +1,1 @@
-set FNM_PATH "/Users/pf1gura/Library/Application Support/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env --use-on-cd | source
-end
+fnm env --corepack-enabled --use-on-cd | source
