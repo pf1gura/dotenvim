5babbae336ff527e76a61b2fddead92311e48b7d
Przemyslaw Figura <github.enlarging980@passinbox.com>
2024-06-14 00:47:28 +0200

    add prettier support

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.blade_formatter,
 				null_ls.builtins.formatting.phpcsfixer,
+				null_ls.builtins.formatting.prettier,
 			},
 		})
 
