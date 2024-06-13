4f483232df2ae7c1f62668750a31720fea3eb484
Przemyslaw Figura <github.enlarging980@passinbox.com>
2024-06-13 23:55:29 +0200

    add react native dependencies config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,11 +9,20 @@ if test (uname) = "Darwin"
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 
 	alias cdd="cd $HOME/Developer"
+	fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 
 	set -gx HOMEBREW_NO_ENV_HINTS true
 
 	fish_add_path "$HOME/.composer/vendor/bin"
-	fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+
+	# set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
+	set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
+	set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
+	set -gx ANDROID_SDK_ROOT "$HOME/Library/Android/sdk"
+	fish_add_path "$ANDROID_HOME/emulator"
+	fish_add_path "$ANDROID_HOME/platform-tools"
+	# fish_add_path "$ANDROID_HOME/cmdline-tools/latest/bin"
+	fish_add_path "$ANDROID_HOME/cmdline-tools/10.0/bin"
 
 	source ~/.orbstack/shell/init.fish 2>/dev/null || :
 end
