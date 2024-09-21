be7cd7caa9a67de23a115a03543f01f57b6c7ce9
Przemyslaw Figura <github.enlarging980@passinbox.com>
2024-09-22 00:54:43 +0200

    Cleanup fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,21 +9,11 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 set -gx HOMEBREW_NO_ENV_HINTS true
 
 alias cdd="cd $HOME/Developer"
+fish_add_path "$HOME/.local/bin"
 fish_add_path "/usr/local/bin"
-fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 
 set -gx EDITOR "nvim"
 set -gx GIT_EDITOR "nvim"
-
-# Android
- set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
- fish_add_path "$ANDROID_HOME/cmdline-tools/latest/bin"
-#set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
-#fish_add_path "$ANDROID_HOME/cmdline-tools/10.0/bin"
-set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
-set -gx ANDROID_SDK_ROOT "$HOME/Library/Android/sdk"
-fish_add_path "$ANDROID_HOME/emulator"
-fish_add_path "$ANDROID_HOME/platform-tools"
 
 # Docker
 source ~/.orbstack/shell/init.fish 2>/dev/null || :
